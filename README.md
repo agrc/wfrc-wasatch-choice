e5651df0f8603adf7cc2329c636478900ba4180d
Scott Davis <stdavis@utah.gov>
2019-07-29 18:57:14 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,59 @@
+# atlas
+
+## AGRC JavaScript Project Boilerplate
+
+[![Build Status](https://travis-ci.com/agrc/atlas.svg?branch=master)](https://travis-ci.com/agrc/atlas)
+
+## To Use
+
+### Step 1 - Boilerplate
+
+Checkout repository and install dependencies
+
+```bash
+git clone https://github.com/agrc/atlas.git
+cd atlas
+npm install
+```
+
+Duplicate the `.env` file and rename it as `.env.development.local` and update the properties with your data
+
+### Step 2 - Develop & Test
+
+Execute `npm start` to start a web server and view the website
+
+Open [`src/App.js`](src/App.js) to view the development version of the app.
+
+Build an awesome app.
+
+Execute `npm test` to run tests
+
+### Step 3 - Optimize
+
+Execute `npm run build` to create an optimized production build
+
+_The files will be placed in `/build`_
+
+Execute `serve -s build` to view the website
+
+### Step 4 - Deploy
+
+One-time tasks:
+
+- [ ] Update the analytics code in `public/index.html`
+- [ ] Create and populate `secrets.json` based on `secrets.sample.json`
+
+Tasks to be completed for each release:
+
+- [ ] Semantically update `.env` property `REACT_APP_VERSION` and `package.json` version
+- [ ] Update `public/changelog.html`
+- [ ] Verify all tests are passing (`npm test`)
+- [ ] Run a build and test (`npm run build`)
+- [ ] Deploy website (`grunt deploy-prod`)
+- [ ] Create release commit (`git commit -m 'chore: Release vx.x.x`)
+- [ ] Create tag (`git tag vx.x.x`)
+- [ ] Push commits and tag to github (`git push origin && git push origin --tags`)
+
+## Notes
+
+A WASM MIME type that is used in the Esri JS API (geometry engine) is defined in [web.config](_src/web.config). This will cause a server error in IIS if you have the same type defined in a parent directory. If you experience this error you will need to remove one of the duplicate MIME type definitions.
---
+++ src/components/esrijs/MapView.js
@@ -0,0 +1,143 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import { loadModules, loadCss } from 'esri-loader';
+import { LayerSelectorContainer, LayerSelector } from '../../components/LayerSelector/LayerSelector';
+import cityExtents from './data/cityExtents.json';
+
+
+export default class ReactMapView extends Component {
+  zoomLevel = 5;
+  displayedZoomGraphic = null;
+  urls = {
+    landownership: 'https://gis.trustlands.utah.gov/server/' +
+      '/rest/services/Ownership/UT_SITLA_Ownership_LandOwnership_WM/FeatureServer/0'
+  };
+
+  render() {
+    return (
+      <div
+        style={{ height: '100%', width: '100%' }}
+        ref={mapViewDiv => {
+          this.mapViewDiv = mapViewDiv;
+        }}
+      />
+    );
+  }
+
+  async componentDidMount() {
+    loadCss('https://js.arcgis.com/4.9/esri/css/main.css');
+    const mapRequires = [
+      'esri/Map',
+      'esri/views/MapView',
+      'esri/layers/FeatureLayer',
+      'esri/geometry/Polygon'
+    ];
+    const selectorRequires = [
+      'esri/layers/support/LOD',
+      'esri/layers/support/TileInfo',
+      'esri/layers/WebTileLayer',
+      'esri/Basemap'
+    ];
+
+    const [Map, MapView, FeatureLayer, Polygon, LOD, TileInfo, WebTileLayer, Basemap] = await loadModules(mapRequires.concat(selectorRequires));
+
+    this.map = new Map();
+
+    // get random city extent
+    const randomExtent = cityExtents[Math.round(Math.random() * (cityExtents.length - 1))];
+    const extent = new Polygon(randomExtent.geometry).extent;
+
+    this.view = new MapView({
+      container: this.mapViewDiv,
+      map: this.map,
+      extent,
+      ui: {
+        components: ['zoom']
+      }
+    });
+
+    this.props.setView(this.view);
+
+    const selectorNode = document.createElement('div');
+    this.view.ui.add(selectorNode, 'top-right');
+
+    const layerSelectorOptions = {
+      view: this.view,
+      quadWord: this.props.discoverKey,
+      baseLayers: ['Hybrid', 'Lite', 'Terrain', 'Topo', 'Color IR'],
+      overlays: ['Address Points', {
+        Factory: FeatureLayer,
+        url: this.urls.landownership,
+        id: 'Land Ownership',
+        opacity: 0.3
+      }],
+      modules: [LOD, TileInfo, WebTileLayer, Basemap]
+    }
+
+    ReactDOM.render(
+      <LayerSelectorContainer>
+        <LayerSelector {...layerSelectorOptions}></LayerSelector>
+      </LayerSelectorContainer>,
+      selectorNode);
+
+    this.view.on('click', this.props.onClick);
+  }
+
+  componentDidUpdate(prevProps) {
+    const currentGraphic = (((this.props || false).zoomToGraphic || false).graphic || false);
+    const previousGraphic = (((prevProps || false).zoomToGraphic || false).graphic || false);
+
+    if (currentGraphic !== previousGraphic && currentGraphic !== false) {
+      const { graphic, level, preserve } = this.props.zoomToGraphic;
+
+      this.zoomTo({
+        target: graphic,
+        zoom: level,
+        preserve: preserve
+      });
+    }
+  }
+
+  async zoomTo(zoomObj) {
+    console.log('app.zoomTo', arguments);
+
+    if (!Array.isArray(zoomObj.target)) {
+      zoomObj.target = [zoomObj.target];
+    }
+
+    if (!zoomObj.zoom) {
+      if (zoomObj.target.every(graphic => graphic.geometry.type === 'point')) {
+        zoomObj = {
+          target: zoomObj.target,
+          zoom: this.view.map.basemap.baseLayers.items[0].tileInfo.lods.length - this.zoomLevel
+        };
+      } else {
+        zoomObj = {
+          target: zoomObj.target
+        };
+      }
+    }
+
+    await this.view.goTo(zoomObj);
+
+    if (this.displayedZoomGraphic) {
+      this.view.graphics.removeMany(this.displayedZoomGraphic);
+    }
+
+    this.displayedZoomGraphic = zoomObj.target;
+
+    this.view.graphics.addMany(zoomObj.target);
+
+    const [watchUtils] = await loadModules(['esri/core/watchUtils']);
+
+    if (!zoomObj.preserve) {
+      watchUtils.once(this.view, 'extent', () => {
+        this.view.graphics.removeAll();
+      });
+    }
+  }
+
+  getView() {
+    return this.view;
+  }
+}
