2b1bc28a10ecf5ee6f750ab5d45106f6b516ff01
Scott Davis <stdavis@utah.gov>
2019-07-30 14:59:04 -0600

    build: fix react build warning about target=blank

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -10,7 +10,12 @@ export default class Header extends PureComponent {
       <div className="app__header">
         <h4 className="header__heading">
           <span>{this.props.title}</span>
-          <a className="heading__version" href="https://github.com/agrc/wfrc/blob/master/CHANGELOG.md" target="_blank" rel="noopener">{this.props.version}</a>
+          <a className="heading__version"
+            href="https://github.com/agrc/wfrc/blob/master/CHANGELOG.md"
+            target="_blank"
+            rel="noopener noreferrer">
+            {this.props.version}
+          </a>
         </h4>
         <Tabs />
         {window.innerWidth >= config.MIN_DESKTOP_WIDTH && <img src={logo} className="heading__img" alt="agrc logo" />}
