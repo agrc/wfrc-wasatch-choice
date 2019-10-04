b421f23cb057164ed42376c2ee070815db6a7406
Scott Davis <stdavis@utah.gov>
2019-10-04 17:04:09 -0600

    fix: make tabs scroll horizontally on smaller screens #20

--- src/components/Tabs/Tabs.js
+++ src/components/Tabs/Tabs.js
@@ -1,6 +1,6 @@
 import React, { useContext } from 'react';
 import { Nav, NavItem, NavLink } from 'reactstrap';
-import './Tabs.css';
+import './Tabs.scss';
 import TabsContext from './TabsContext';
 import config from '../../config';
 
@@ -10,18 +10,20 @@ export default props => {
   const onClick = index => setCurrentTab(index);
 
   return (
-    <Nav tabs className="tabs">
-      { config.tabs.map(({name}, index) => {
-        index = index + '';
+    <div className="tabs">
+      <Nav tabs>
+        { config.tabs.map(({name}, index) => {
+          index = index + '';
 
-        return (
-          <NavItem key={index}>
-            <NavLink className={(currentTabIndex + '' === index) ? 'active' : null} onClick={onClick.bind(this, index)}>
-              {name}
-            </NavLink>
-          </NavItem>
-        );
-      }) }
-    </Nav>
+          return (
+            <NavItem key={index}>
+              <NavLink className={(currentTabIndex + '' === index) ? 'active' : null} onClick={onClick.bind(this, index)}>
+                {name}
+              </NavLink>
+            </NavItem>
+          );
+        }) }
+      </Nav>
+    </div>
   );
 }
