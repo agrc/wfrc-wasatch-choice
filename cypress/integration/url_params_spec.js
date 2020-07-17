904a7edd817b2113d86323495004f07ff2cdb2f9
stdavis <stdavis@utah.gov>
2020-07-17 10:23:08 -0600

    feat: add url param tests for extent includes refator of get map extent command

--- cypress/integration/url_params_spec.js
+++ cypress/integration/url_params_spec.js
@@ -4,7 +4,7 @@ describe('url params', () => {
 
     cy.findByTestId('center-container').should('have.css', 'left', '0px');
   });
-  it('currentTab activates the correct tab', () => {
+  it('currentTabId activates the correct tab', () => {
     cy.loadApp('http://localhost:3000/#currentTabId=land-use');
 
     cy.findByLabelText('Land Use Tab').should('have.class', 'active');
@@ -12,5 +12,10 @@ describe('url params', () => {
     cy.window().then(win => {
       expect(win.currentMapName).to.equal('Land Use');
     });
+  });
+  it('scale, x, & y params set the map extent', () => {
+    cy.loadApp('http://localhost:3000/#currentTab=land-use&currentTabId=vision&scale=72224&sideBarClosed=true&x=-12461939&y=4976704');
+
+    cy.getMapExtent().then(extent => expect(extent).to.equal('{"spatialReference":{"wkid":3857},"xmin":-12471493.628535634,"ymin":4971276.97099176,"xmax":-12452384.371464366,"ymax":4982131.02900824}'));
   });
 });
