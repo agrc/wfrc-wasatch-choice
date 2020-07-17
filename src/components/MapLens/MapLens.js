289d3377c872ec99f599be9404067ff4830d689e
stdavis <stdavis@utah.gov>
2020-07-17 10:13:38 -0600

    feat: add a few url param tests

--- src/components/MapLens/MapLens.js
+++ src/components/MapLens/MapLens.js
@@ -7,7 +7,7 @@ import { Button } from 'reactstrap';
 
 export default props => {
   return (
-    <div id="centerContainer" className={'map-lens map-lens--with-border' + (props.sideBarOpen ? ' map-lens--side-bar-open' : '')}>
+    <div id="centerContainer" data-testid='center-container' className={'map-lens map-lens--with-border' + (props.sideBarOpen ? ' map-lens--side-bar-open' : '')}>
       <Button size="sm" color="info" className="map-lens__sidebar btn btn-default btn-xs" onClick={props.toggleSidebar}>
         {props.sideBarOpen ? <FontAwesomeIcon icon={faChevronLeft} size='xs' /> : <FontAwesomeIcon icon={faChevronLeft} size='xs' flip='horizontal' /> }
       </Button>
