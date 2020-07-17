ccb7aa77fd6adbf272e71df467101cff26aef613
stdavis <stdavis@utah.gov>
2020-07-17 13:27:41 -0600

    feat: manage current tab ids as state in tabs component With local storage value as default if present.

--- src/components/Tabs/Tabs.js
+++ src/components/Tabs/Tabs.js
@@ -2,17 +2,19 @@ import React from 'react';
 import { Nav, NavItem, NavLink } from 'reactstrap';
 import './Tabs.scss';
 import { useCurrentTabConfig } from './TabsContext';
-import config, { getCurrentTabIds } from '../../config';
+import config, { getDefaultCurrentTabIds } from '../../config';
 
 
 export default props => {
   const [ currentTabConfig, setCurrentTabConfig ] = useCurrentTabConfig();
+  const initialTabIds = localStorage.getItem('currentTabIds') || getDefaultCurrentTabIds();
+  const [ currentTabIds, setCurrentTabIds ] = React.useState(initialTabIds);
   const onClick = id => setCurrentTabConfig({id, ...config.tabInfos[id]});
 
   return (
     <div className="tabs">
       <Nav tabs>
-        { getCurrentTabIds().map(id => {
+        { currentTabIds.map(id => {
           const tabInfo = config.tabInfos[id];
 
           return (
