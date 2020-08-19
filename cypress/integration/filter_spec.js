e54df37227e65106781070ee52b62a9e1de77032
stdavis <stdavis@utah.gov>
2020-08-19 09:51:33 -0600

    chore: add filter integration tests Related to #56

---
+++ cypress/integration/filter_spec.js
@@ -0,0 +1,30 @@
+describe('filter', () => {
+  it('reset button resets all of the controls', () => {
+    cy.loadApp();
+
+    cy.get('.filter .group-container input').first().click();
+    cy.get('.filter .child-checkbox-container input').last().click();
+
+    cy.findByText(/reset/i).first().click();
+
+    cy.get('.filter .group-container input').check();
+    cy.get('.filter .child-checkbox-container input').check();
+  });
+  it('turning off checkboxes reduces the number of layers', () => {
+    cy.loadApp();
+
+    let getVisibleLayers;
+    let originalVisibleLayers;
+    cy.window().then(win => {
+      getVisibleLayers = win.getVisibleLayers;
+
+      originalVisibleLayers = getVisibleLayers();
+    });
+
+    cy.get('.filter .group-container input').first().click();
+
+    cy.waitForMapLoaded().then(() => {
+      expect(getVisibleLayers().length).below(originalVisibleLayers.length);
+    });
+  });
+});
