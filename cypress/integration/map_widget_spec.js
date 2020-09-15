e0e3142f444ed89ae9c9846a2e2dd5ee29cef557
stdavis <stdavis@utah.gov>
2020-09-15 17:42:11 -0600

    chore: add map widget cypress tests
    Related to #56

---
+++ cypress/integration/map_widget_spec.js
@@ -0,0 +1,25 @@
+describe('map-widget', () => {
+  it('button toggles pane visibility', () => {
+    cy.loadApp();
+
+    cy.findByText(/filter/i).should('be.visible');
+
+    cy.get('[title="Filter"] > .svg-inline--fa').click();
+
+    cy.findByText(/filter/i).should('not.be.visible');
+
+    cy.get('[title="Filter"] > .svg-inline--fa').click();
+
+    cy.findByText(/filter/i).should('be.visible');
+  });
+  it('close button closes pane', () => {
+    cy.loadApp();
+
+    cy.findByText(/filter/i).should('be.visible');
+
+    cy.get('.map-widget-card > .card-header > .buttons-container > .close > span').click({ multiple: true });
+
+    cy.findByText(/filter/i).should('not.be.visible');
+    cy.findByText(/project information/i).should('not.be.visible');
+  });
+});
