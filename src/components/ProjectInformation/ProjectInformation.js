90937c382e67ebaba1c0d8adab7fe62ec203276d
stdavis <stdavis@utah.gov>
2020-09-18 10:22:26 -0600

    feat: i18n for project info widget
    #51

--- src/components/ProjectInformation/ProjectInformation.js
+++ src/components/ProjectInformation/ProjectInformation.js
@@ -2,15 +2,18 @@ import React from 'react';
 import Details from './Details';
 import './ProjectInformation.scss';
 import Loader from 'react-loader-spinner';
+import { useSpecialTranslation } from '../../i18n';
 
 
 export default props => {
   console.log('ProjectInformation');
 
+  const t = useSpecialTranslation();
+
   return (
     <div className="project-information">
       { props.graphics.length === 0 && !props.showLoader &&
-        <p>Click on a feature for more information</p> }
+        <p>{t('trans:projectInformationPrompt')}</p> }
       { props.showLoader && <Loader type="Oval" className="loader" /> }
       { props.graphics.map((graphic, index) =>
         <Details key={index} graphic={graphic} highlightGraphic={props.highlightGraphic} />) }
