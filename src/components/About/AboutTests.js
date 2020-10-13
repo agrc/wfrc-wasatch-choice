11c827858fb9e07819b37785ae583d23678f266e
stdavis <stdavis@utah.gov>
2020-10-13 08:07:52 -0600

    fix: replace vision with new content and remove temporary container

--- src/components/About/AboutTests.js
+++ src/components/About/AboutTests.js
@@ -9,10 +9,6 @@ import '../../App.scss';
 const AboutTests = () => {
   return (
     <div className="about-tests">
-      <Sidebar isOpen={true}>
-        <h3>{'vision-new'}</h3>
-        <About testTabId={'vision-new'}/>
-      </Sidebar>
       { Object.keys(config.mapInfos).map((tabId, index) =>
         <Sidebar isOpen={true} key={index}>
           <h3>{tabId}</h3>
