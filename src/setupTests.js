a4f30b777264695a7b2e0bfb1b94952af1d0c6c0
stdavis <stdavis@utah.gov>
2021-02-22 17:24:27 -0700

    tests: force device detection

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,5 +1,8 @@
 import appConfig from '../public/config.json';
 import configSchema from '../public/config.schema.json';
 import { setConfigs } from './config';
+import * as rdd from 'react-device-detect';
 
 setConfigs(appConfig, configSchema);
+
+rdd.isMobile = false;
