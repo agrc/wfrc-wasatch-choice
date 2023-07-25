1bcd50852c74decff3b94905d75af5b404af5f6d
stdavis <stdavis@utah.gov>
2023-07-25 14:49:05 -0600

    chore: wire up config files to vite auto-reload

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,28 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
+function watchConfigFiles() {
+  return {
+    name: 'watch-config-files',
+    enforce: 'post',
+    handleHotUpdate({ file, server }) {
+      if (file.endsWith('.json') || file.endsWith('about.html')) {
+        server.ws.send({
+          type: 'full-reload',
+          path: '*',
+        });
+      }
+    },
+  };
+}
+
 // https://vitejs.dev/config/
 export default defineConfig({
   base: './',
   define: {
     'import.meta.env.PACKAGE_VERSION': JSON.stringify(process.env.npm_package_version),
   },
-  plugins: [react()],
+  plugins: [react(), watchConfigFiles()],
   test: {
     setupFiles: ['./setupTests.js'],
     environment: 'happy-dom',
