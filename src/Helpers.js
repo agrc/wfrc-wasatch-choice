cfbafc2aedb79da7557f1cc93a153407a6d02639
stdavis <stdavis@utah.gov>
2023-08-25 17:02:16 -0600

    chore: clean up unused module 🧹

--- src/Helpers.js
+++
@@ -1,8 +0,0 @@
-const toQueryString = (obj) => {
-  return Object.keys(obj)
-    .map((key) => encodeURIComponent(key) + '=' + encodeURIComponent(obj[key]))
-    .join('&')
-    .replace(/%20/g, '+');
-};
-
-export default { toQueryString };
